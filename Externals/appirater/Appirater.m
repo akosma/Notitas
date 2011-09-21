897a130eae5b1231f86d29ffd4e1838ba8c7c451
Adrian Kosmaczewski <adrian@akosma.com>
2011-09-21 08:33:40 +0200

    Updated the Appirater library

--- Externals/appirater/Appirater.m
+++ Externals/appirater/Appirater.m
@@ -1,7 +1,7 @@
 /*
  This file is part of Appirater.
  
- Copyright (c) 2009, Arash Payan
+ Copyright (c) 2010, Arash Payan
  All rights reserved.
  
  Permission is hereby granted, free of charge, to any person
@@ -31,23 +31,30 @@
  *
  * Created by Arash Payan on 9/5/09.
  * http://arashpayan.com
- * Copyright 2009 Arash Payan. All rights reserved.
+ * Copyright 2010 Arash Payan. All rights reserved.
  */
 
 #import "Appirater.h"
 #import <SystemConfiguration/SCNetworkReachability.h>
 #include <netinet/in.h>
 
-NSString *const kAppiraterLaunchDate				= @"kAppiraterLaunchDate";
-NSString *const kAppiraterLaunchCount				= @"kAppiraterLaunchCount";
+NSString *const kAppiraterFirstUseDate				= @"kAppiraterFirstUseDate";
+NSString *const kAppiraterUseCount					= @"kAppiraterUseCount";
+NSString *const kAppiraterSignificantEventCount		= @"kAppiraterSignificantEventCount";
 NSString *const kAppiraterCurrentVersion			= @"kAppiraterCurrentVersion";
 NSString *const kAppiraterRatedCurrentVersion		= @"kAppiraterRatedCurrentVersion";
 NSString *const kAppiraterDeclinedToRate			= @"kAppiraterDeclinedToRate";
+NSString *const kAppiraterReminderRequestDate		= @"kAppiraterReminderRequestDate";
 
-NSString *templateReviewURL = @"http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
+NSString *templateReviewURL = @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=APP_ID";
+
 
 @interface Appirater (hidden)
 - (BOOL)connectedToNetwork;
++ (Appirater*)sharedInstance;
+- (void)showRatingAlert;
+- (BOOL)ratingConditionsHaveBeenMet;
+- (void)incrementUseCount;
 @end
 
 @implementation Appirater (hidden)
@@ -83,26 +90,72 @@ NSString *templateReviewURL = @"http://itunes.apple.com/WebObjects/MZStore.woa/w
     return ((isReachable && !needsConnection) || nonWiFi) ? (testConnection ? YES : NO) : NO;
 }
 
-@end
-
-
-@implementation Appirater
-
-+ (void)appLaunched {
-	Appirater *appirater = [[Appirater alloc] init];
-	[NSThread detachNewThreadSelector:@selector(_appLaunched) toTarget:appirater withObject:nil];
-}
-
-- (void)_appLaunched {
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
-	if (APPIRATER_DEBUG)
++ (Appirater*)sharedInstance {
+	static Appirater *appirater = nil;
+	if (appirater == nil)
 	{
-		[self performSelectorOnMainThread:@selector(showPrompt) withObject:nil waitUntilDone:NO];
-		
-		return;
+		@synchronized(self) {
+			if (appirater == nil) {
+				appirater = [[Appirater alloc] init];
+                [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillResignActive) name:@"UIApplicationWillResignActiveNotification" object:nil];
+            }
+        }
 	}
 	
+	return appirater;
+}
+
+- (void)showRatingAlert {
+	UIAlertView *alertView = [[[UIAlertView alloc] initWithTitle:APPIRATER_MESSAGE_TITLE
+														 message:APPIRATER_MESSAGE
+														delegate:self
+											   cancelButtonTitle:APPIRATER_CANCEL_BUTTON
+											   otherButtonTitles:APPIRATER_RATE_BUTTON, APPIRATER_RATE_LATER, nil] autorelease];
+	self.ratingAlert = alertView;
+	[alertView show];
+}
+
+- (BOOL)ratingConditionsHaveBeenMet {
+	if (APPIRATER_DEBUG)
+		return YES;
+	
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	
+	NSDate *dateOfFirstLaunch = [NSDate dateWithTimeIntervalSince1970:[userDefaults doubleForKey:kAppiraterFirstUseDate]];
+	NSTimeInterval timeSinceFirstLaunch = [[NSDate date] timeIntervalSinceDate:dateOfFirstLaunch];
+	NSTimeInterval timeUntilRate = 60 * 60 * 24 * APPIRATER_DAYS_UNTIL_PROMPT;
+	if (timeSinceFirstLaunch < timeUntilRate)
+		return NO;
+	
+	// check if the app has been used enough
+	int useCount = [userDefaults integerForKey:kAppiraterUseCount];
+	if (useCount <= APPIRATER_USES_UNTIL_PROMPT)
+		return NO;
+	
+	// check if the user has done enough significant events
+	int sigEventCount = [userDefaults integerForKey:kAppiraterSignificantEventCount];
+	if (sigEventCount <= APPIRATER_SIG_EVENTS_UNTIL_PROMPT)
+		return NO;
+	
+	// has the user previously declined to rate this version of the app?
+	if ([userDefaults boolForKey:kAppiraterDeclinedToRate])
+		return NO;
+	
+	// has the user already rated the app?
+	if ([userDefaults boolForKey:kAppiraterRatedCurrentVersion])
+		return NO;
+	
+	// if the user wanted to be reminded later, has enough time passed?
+	NSDate *reminderRequestDate = [NSDate dateWithTimeIntervalSince1970:[userDefaults doubleForKey:kAppiraterReminderRequestDate]];
+	NSTimeInterval timeSinceReminderRequest = [[NSDate date] timeIntervalSinceDate:reminderRequestDate];
+	NSTimeInterval timeUntilReminder = 60 * 60 * 24 * APPIRATER_TIME_BEFORE_REMINDING;
+	if (timeSinceReminderRequest < timeUntilReminder)
+		return NO;
+	
+	return YES;
+}
+
+- (void)incrementUseCount {
 	// get the app's version
 	NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleVersionKey];
 	
@@ -120,102 +173,204 @@ NSString *templateReviewURL = @"http://itunes.apple.com/WebObjects/MZStore.woa/w
 	
 	if ([trackingVersion isEqualToString:version])
 	{
-		// get the launch date
-		NSTimeInterval timeInterval = [userDefaults doubleForKey:kAppiraterLaunchDate];
+		// check if the first use date has been set. if not, set it.
+		NSTimeInterval timeInterval = [userDefaults doubleForKey:kAppiraterFirstUseDate];
 		if (timeInterval == 0)
 		{
 			timeInterval = [[NSDate date] timeIntervalSince1970];
-			[userDefaults setDouble:timeInterval forKey:kAppiraterLaunchDate];
+			[userDefaults setDouble:timeInterval forKey:kAppiraterFirstUseDate];
 		}
 		
-		NSTimeInterval secondsSinceLaunch = [[NSDate date] timeIntervalSinceDate:[NSDate dateWithTimeIntervalSince1970:timeInterval]];
-		double secondsUntilPrompt = 60 * 60 * 24 * DAYS_UNTIL_PROMPT;
-		
-		// get the launch count
-		int launchCount = [userDefaults integerForKey:kAppiraterLaunchCount];
-		launchCount++;
-		[userDefaults setInteger:launchCount forKey:kAppiraterLaunchCount];
+		// increment the use count
+		int useCount = [userDefaults integerForKey:kAppiraterUseCount];
+		useCount++;
+		[userDefaults setInteger:useCount forKey:kAppiraterUseCount];
 		if (APPIRATER_DEBUG)
-			NSLog(@"APPIRATER Launch count: %d", launchCount);
-		
-		// have they previously declined to rate this version of the app?
-		BOOL declinedToRate = [userDefaults boolForKey:kAppiraterDeclinedToRate];
-		
-		// have they already rated the app?
-		BOOL ratedApp = [userDefaults boolForKey:kAppiraterRatedCurrentVersion];
-		
-		if (secondsSinceLaunch > secondsUntilPrompt &&
-			launchCount > LAUNCHES_UNTIL_PROMPT &&
-			!declinedToRate &&
-			!ratedApp)
-		{
-			if ([self connectedToNetwork])	// check if they can reach the app store
-				[self performSelectorOnMainThread:@selector(showPrompt) withObject:nil waitUntilDone:NO];
-		}
+			NSLog(@"APPIRATER Use count: %d", useCount);
 	}
 	else
 	{
 		// it's a new version of the app, so restart tracking
 		[userDefaults setObject:version forKey:kAppiraterCurrentVersion];
-		[userDefaults setDouble:[[NSDate date] timeIntervalSince1970] forKey:kAppiraterLaunchDate];
-		[userDefaults setInteger:1 forKey:kAppiraterLaunchCount];
+		[userDefaults setDouble:[[NSDate date] timeIntervalSince1970] forKey:kAppiraterFirstUseDate];
+		[userDefaults setInteger:1 forKey:kAppiraterUseCount];
+		[userDefaults setInteger:0 forKey:kAppiraterSignificantEventCount];
 		[userDefaults setBool:NO forKey:kAppiraterRatedCurrentVersion];
 		[userDefaults setBool:NO forKey:kAppiraterDeclinedToRate];
+		[userDefaults setDouble:0 forKey:kAppiraterReminderRequestDate];
 	}
-
 	
 	[userDefaults synchronize];
+}
+
+- (void)incrementSignificantEventCount {
+	// get the app's version
+	NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleVersionKey];
+	
+	// get the version number that we've been tracking
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	NSString *trackingVersion = [userDefaults stringForKey:kAppiraterCurrentVersion];
+	if (trackingVersion == nil)
+	{
+		trackingVersion = version;
+		[userDefaults setObject:version forKey:kAppiraterCurrentVersion];
+	}
+	
+	if (APPIRATER_DEBUG)
+		NSLog(@"APPIRATER Tracking version: %@", trackingVersion);
+	
+	if ([trackingVersion isEqualToString:version])
+	{
+		// check if the first use date has been set. if not, set it.
+		NSTimeInterval timeInterval = [userDefaults doubleForKey:kAppiraterFirstUseDate];
+		if (timeInterval == 0)
+		{
+			timeInterval = [[NSDate date] timeIntervalSince1970];
+			[userDefaults setDouble:timeInterval forKey:kAppiraterFirstUseDate];
+		}
+		
+		// increment the significant event count
+		int sigEventCount = [userDefaults integerForKey:kAppiraterSignificantEventCount];
+		sigEventCount++;
+		[userDefaults setInteger:sigEventCount forKey:kAppiraterSignificantEventCount];
+		if (APPIRATER_DEBUG)
+			NSLog(@"APPIRATER Significant event count: %d", sigEventCount);
+	}
+	else
+	{
+		// it's a new version of the app, so restart tracking
+		[userDefaults setObject:version forKey:kAppiraterCurrentVersion];
+		[userDefaults setDouble:0 forKey:kAppiraterFirstUseDate];
+		[userDefaults setInteger:0 forKey:kAppiraterUseCount];
+		[userDefaults setInteger:1 forKey:kAppiraterSignificantEventCount];
+		[userDefaults setBool:NO forKey:kAppiraterRatedCurrentVersion];
+		[userDefaults setBool:NO forKey:kAppiraterDeclinedToRate];
+		[userDefaults setDouble:0 forKey:kAppiraterReminderRequestDate];
+	}
+	
+	[userDefaults synchronize];
+}
+
+@end
+
+
+@interface Appirater ()
+- (void)hideRatingAlert;
+@end
+
+@implementation Appirater
+
+@synthesize ratingAlert;
+
+- (void)incrementAndRate:(NSNumber*)_canPromptForRating {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	[self incrementUseCount];
+	
+	if ([_canPromptForRating boolValue] == YES &&
+		[self ratingConditionsHaveBeenMet] &&
+		[self connectedToNetwork])
+	{
+		[self performSelectorOnMainThread:@selector(showRatingAlert) withObject:nil waitUntilDone:NO];
+	}
 	
 	[pool release];
 }
 
-- (void)showPrompt {
-	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:APPIRATER_MESSAGE_TITLE
-														message:APPIRATER_MESSAGE
-													   delegate:self
-											  cancelButtonTitle:APPIRATER_CANCEL_BUTTON
-											  otherButtonTitles:APPIRATER_RATE_BUTTON, APPIRATER_RATE_LATER, nil];
-	[alertView show];
+- (void)incrementSignificantEventAndRate:(NSNumber*)_canPromptForRating {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	[self incrementSignificantEventCount];
+	
+	if ([_canPromptForRating boolValue] == YES &&
+		[self ratingConditionsHaveBeenMet] &&
+		[self connectedToNetwork])
+	{
+		[self performSelectorOnMainThread:@selector(showRatingAlert) withObject:nil waitUntilDone:NO];
+	}
+	
+	[pool release];
+}
+
++ (void)appLaunched {
+	[Appirater appLaunched:YES];
+}
+
++ (void)appLaunched:(BOOL)canPromptForRating {
+	NSNumber *_canPromptForRating = [[NSNumber alloc] initWithBool:canPromptForRating];
+	[NSThread detachNewThreadSelector:@selector(incrementAndRate:)
+							 toTarget:[Appirater sharedInstance]
+						   withObject:_canPromptForRating];
+	[_canPromptForRating release];
+}
+
+- (void)hideRatingAlert {
+	if (self.ratingAlert.visible) {
+		if (APPIRATER_DEBUG)
+			NSLog(@"APPIRATER Hiding Alert");
+		[self.ratingAlert dismissWithClickedButtonIndex:-1 animated:NO];
+	}	
+}
+
++ (void)appWillResignActive {
+	if (APPIRATER_DEBUG)
+		NSLog(@"APPIRATER appWillResignActive");
+	[[Appirater sharedInstance] hideRatingAlert];
+}
+
++ (void)appEnteredForeground:(BOOL)canPromptForRating {
+	NSNumber *_canPromptForRating = [[NSNumber alloc] initWithBool:canPromptForRating];
+	[NSThread detachNewThreadSelector:@selector(incrementAndRate:)
+							 toTarget:[Appirater sharedInstance]
+						   withObject:_canPromptForRating];
+	[_canPromptForRating release];
+}
+
++ (void)userDidSignificantEvent:(BOOL)canPromptForRating {
+	NSNumber *_canPromptForRating = [[NSNumber alloc] initWithBool:canPromptForRating];
+	[NSThread detachNewThreadSelector:@selector(incrementSignificantEventAndRate:)
+							 toTarget:[Appirater sharedInstance]
+						   withObject:_canPromptForRating];
+	[_canPromptForRating release];
+}
+
++ (void)rateApp {
+#if TARGET_IPHONE_SIMULATOR
+	NSLog(@"APPIRATER NOTE: iTunes App Store is not supported on the iOS simulator. Unable to open App Store page.");
+#else
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	NSString *reviewURL = [templateReviewURL stringByReplacingOccurrencesOfString:@"APP_ID" withString:[NSString stringWithFormat:@"%d", APPIRATER_APP_ID]];
+	[userDefaults setBool:YES forKey:kAppiraterRatedCurrentVersion];
+	[userDefaults synchronize];
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:reviewURL]];
+#endif
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
 	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-	
-	//if (buttonIndex == 1)
-//	{
-//		NSString *reviewURL = [templateReviewURL stringByReplacingOccurrencesOfString:@"APP_ID" withString:[NSString stringWithFormat:@"%d", APPIRATER_APP_ID]];
-//		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:reviewURL]];
-//		
-//		[userDefaults setBool:YES forKey:kAppiraterRatedCurrentVersion];
-//	}
 	
 	switch (buttonIndex) {
 		case 0:
 		{
 			// they don't want to rate it
 			[userDefaults setBool:YES forKey:kAppiraterDeclinedToRate];
+			[userDefaults synchronize];
 			break;
 		}
 		case 1:
 		{
 			// they want to rate it
-			NSString *reviewURL = [templateReviewURL stringByReplacingOccurrencesOfString:@"APP_ID" withString:[NSString stringWithFormat:@"%d", APPIRATER_APP_ID]];
-			[[UIApplication sharedApplication] openURL:[NSURL URLWithString:reviewURL]];
-			
-			[userDefaults setBool:YES forKey:kAppiraterRatedCurrentVersion];
+			[Appirater rateApp];
 			break;
 		}
 		case 2:
 			// remind them later
+			[userDefaults setDouble:[[NSDate date] timeIntervalSince1970] forKey:kAppiraterReminderRequestDate];
+			[userDefaults synchronize];
 			break;
 		default:
 			break;
 	}
-	
-	[userDefaults synchronize];
-	
-	[alertView release];
-	[self release];
 }
 
 @end
